1e7606a80b07bddb14b78f0f9a29af4cdf94933d
smplyamzng <kharmon9452@gmail.com>
2019-10-08 09:58:14 -0400

    changed nav graphics

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -14,7 +14,7 @@ Spree.config do |config|
   # Uncomment to stop tracking inventory levels in the application
 #   # config.track_inventory_levels = false
 
-	config.admin_interface_logo = 'https://takecareassets.s3.amazonaws.com/logo_white.png'
+	config.admin_interface_logo = 'https://takecareassets.s3.amazonaws.com/tc_nav_white.png'
 	
 
 
